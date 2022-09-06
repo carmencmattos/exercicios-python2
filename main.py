03ea6e8599080f3c4e160823e2f5ae9e5860c5fe
carmencmattos <99370978+carmencmattos@users.noreply.github.com>
2022-09-06 16:07:04 -0300

    Exercício 6

---
+++ main.py
@@ -0,0 +1,8 @@
+""" Crie um arquivo main.py, importe a classe “Quadrado”, crie uma nova instância e acesse seus métodos. """
+
+from ex5 import Quadrado
+
+quadrado = Quadrado(16)
+
+print (f"A área do quadrado é igual a {quadrado.area_quadrado()}")
+print (f"O perímetro do quadrado é igual a {quadrado.perimetro_quadrado()}")
